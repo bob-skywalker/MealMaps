52b09e01887db37d25a1a0fb9e79f8d80497597e
bobo <q463250938@gmail.com>
2023-07-20 19:15:39 -0700

    Initial Commit

---
+++ MealMaps/MealMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMapsApp.swift
+//  MealMaps
+//
+//  Created by Bo Zhong on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MealMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
